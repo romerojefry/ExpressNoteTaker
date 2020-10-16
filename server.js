52e1b173a81e2ef459b2904620b765bd21119e92
Jefry Romero <romerojefry@icloud.com>
2020-10-16 00:50:25 -0700

    adding beginng routes

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const path = require ("path");
 
 //browser and heroku 
 const PORT = process.env.PORT || 3000;
+
+
+//routes
+
+require("./routes/htmlRoute")(app);
+require("./routes/apiRoute")(app);
+
