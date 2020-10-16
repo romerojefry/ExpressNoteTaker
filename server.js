9e171ef6472536d0b1890a8ca4d7da1ff789e197
Jefry Romero <romerojefry@icloud.com>
2020-10-16 14:46:37 -0700

    adding script

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ notesData= [];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.joint(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 
