4c9df48a876f2556bf0f80b9fa22214242848b38
Jefry Romero <romerojefry@icloud.com>
2020-10-16 01:28:50 -0700

    setting up api array of object

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// my dependencies
 const fs = require("fs");
 const express = require("express");
 const path = require ("path");
 
 
-//browser and heroku 
+//browser and heroku and setting up express
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 //adding data araay
@@ -18,7 +20,18 @@ app.use(express.static(path.joint(__dirname, "public")));
 require("./routes/htmlRoute")(app);
 require("./routes/apiRoute")(app);
 
-//server 
+//
+app.get("/api/notes", function(req, res){
+    notesData =fs.readFileSync("Develop/db/db.json", "utf8");
+    //to make it where notes is an array of objects
+    notesData = JSON.parse(notesData);
+    
+//sent through browser    
+res.json(notesData);
+});
+
+
+//server start listener
 app.listen(PORT, function() {
     console.log("app is listening" = PORT);
-})
+});
