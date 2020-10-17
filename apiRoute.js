64f989010c29fd7809a7d6ab433680a5c81a8a3a
Jefry Romero <romerojefry@icloud.com>
2020-10-16 21:57:06 -0700

    switch notes data to fs.readfile

--- apiRoute.js
+++ apiRoute.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const notesData= []; 
+const notesData= fs.readFileSync("db.json", "utf8"); 
 
 module.exports = function(app){
     
     app.get("/api/notes", function(req, res){
-    notesData =fs.readFileSync("db.json", "utf8");
+    
     //to make it where notes is an array of objects
     notesData = JSON.parse(notesData);
     
