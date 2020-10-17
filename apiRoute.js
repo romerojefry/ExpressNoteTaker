6c5d595075763fee19ca3bdae01381367efbb653
Jefry Romero <romerojefry@icloud.com>
2020-10-16 23:26:57 -0700

    final updates

--- apiRoute.js
+++ apiRoute.js
@@ -1,14 +1,29 @@
 const fs = require("fs");
-const notesData= fs.readFileSync("db.json", "utf8"); 
+const path = require("path");
+const notesData =fs.readFileSync("db.json", "utf8");
 
 module.exports = function(app){
-    
-    app.get("/api/notes", function(req, res){
-    
+
+
+app.get("/api/notes", function(req, res){
+    notesData =fs.readFileSync("db.json", "utf8");
     //to make it where notes is an array of objects
     notesData = JSON.parse(notesData);
     
-    //sent through browser    
-    res.json(notesData);
-    });
+//sent through browser    
+res.json(notesData);
+});
+
+//adding notes
+app.post("/api/notes", function(req,res) {
+    var newNote = req.body;
+    fs.readFile("db.json", "utf8", function(notesData)
+ });
+
+ //delete
+ app.delete("/api/notes/:id", function(req, res)
+    fs,writeFile("db.json", JSON.stringify(notesData),function(error){
+        if(err), throw(err);
+        console.log("deleted")
+    })
 }
